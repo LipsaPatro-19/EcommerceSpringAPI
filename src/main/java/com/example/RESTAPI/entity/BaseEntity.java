d19693b66114b15337405932e7851dd5634c349a
Lipsa <samanamallik357@gmail.com>
2025-07-18 17:25:03 +0530

    added repository and comments and modified properties

--- src/main/java/com/example/RESTAPI/entity/BaseEntity.java
+++ src/main/java/com/example/RESTAPI/entity/BaseEntity.java
@@ -13,14 +13,18 @@ import java.time.Instant;
 
 //MappedSuperclass is a JPA annotation used to define the base class that contains the common fields
 //which will be inherited and mapped into child entity tables
+//@entity > it gets own table after running the application
+//But @MappedSuperclass doesn't get its own table
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener.class) //this is enabling the auditing of all query logs like last date
+//modified date, for this to work we should add a annotation @EnableJpaAuditing in the main class
 @Getter
 @Setter
 public class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//means automatically generate like id and
+    // increment when new entry comes
     private long id;
 
     @CreatedDate
@@ -31,6 +35,7 @@ public class BaseEntity {
     @Column(nullable = false)
     private Instant updatedAt;
 
+    //This method is automatically called before the entity is saved for the first time
     @PrePersist
     public void onCreate(){
         Instant now= Instant.now();
@@ -38,6 +43,7 @@ public class BaseEntity {
         this.updatedAt=now;
     }
 
+    //This will get called when we are updating any entry
     @PreUpdate
     public void onUpdate(){
         this.updatedAt=Instant.now();
--- src/main/java/com/example/RESTAPI/entity/Product.java
+++ src/main/java/com/example/RESTAPI/entity/Product.java
@@ -9,14 +9,14 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Product {
+public class Product extends BaseEntity{
     private String image;
     private String color;
     private Integer price;
     private String description;
     private Integer discount;
     private String model;
-    private Integer id;
+    private long id;
     private String title;
     private String category;
     private String brand;
